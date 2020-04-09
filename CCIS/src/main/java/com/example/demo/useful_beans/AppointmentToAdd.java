6bc62ee519e32bea67f3a216e0e1d861966f6785
Vukasin Jokic <vukasinj98@gmail.com>
2020-04-09 14:50:03 +0200

    Fixed small bug

--- CCIS/src/main/java/com/example/demo/useful_beans/AppointmentToAdd.java
+++ CCIS/src/main/java/com/example/demo/useful_beans/AppointmentToAdd.java
@@ -3,6 +3,7 @@ package com.example.demo.useful_beans;
 public class AppointmentToAdd {
     public float price;
 
+    public AppointmentToAdd(){}
     public AppointmentToAdd(float price) {
         this.price = price;
     }
