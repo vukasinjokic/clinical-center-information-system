70e50372c0b9d2784e2accde2a5e12196a880042
niki299 <nstojanovic998@gmail.com>
2020-04-09 14:09:29 +0200

    Some changes in login form

--- CCIS/src/main/java/com/example/demo/service/LoginService.java
+++ CCIS/src/main/java/com/example/demo/service/LoginService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Doctor;
 import com.example.demo.model.User;
 import com.example.demo.useful_beans.UserToLogin;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,14 @@ import java.util.Optional;
 @Service
 public class LoginService {
 
-    private HashSet<User> users = new HashSet<User>();
+    private HashSet<User> users;
+
+    public LoginService() {
+        User doctor = new Doctor("nikola@gmail.com","123","Nikola","Stojanovic","Pwuskinova 4",
+                "Beograd","Srbija","0111111111","12");
+        this.users = new HashSet<User>();
+        users.add(doctor);
+    }
 
     public void setUsers(HashSet<User> users) {
         this.users = users;
@@ -17,9 +25,9 @@ public class LoginService {
 
     public boolean login(UserToLogin userToLogin){
         Optional<User> user;
-        user = users.stream().filter(user1 -> user1.getEmail().equals(userToLogin)
+        user = users.stream().filter(user1 -> user1.getEmail().equals(userToLogin.email)
             && user1.getPassword().equals(userToLogin.password))
-                .findFirst();
+                .findAny();
         if(user.isPresent())
             return true;
 
