c976ef62742ced81c0e7031187e05d49edd9342c
Nemanja Jevtic <njevtic22@gmail.com>
2020-05-26 21:56:36 +0200

    User story: #TUS-10 Task-4, Task-5. Route navigation guards done.

---
+++ client/src/components/ReturnHome.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-btn
+        class="blue-grey darken-2 white--text"
+        @click="returnHome">
+            Nazad na početnu stranicu
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ReturnHome",
+
+    methods: {
+        returnHome() {
+            const user_role = localStorage.getItem("user_role");
+            if (user_role === "ROLE_CLINIC_CENTER_ADMIN") {
+                this.$router.push({
+                    name: "ClinicCenterAdminPage"
+                });
+            } else if (user_role === "ROLE_CLINIC_ADMIN") {
+                this.$router.push({
+                    name: "ClinicAdminPage"
+                });
+            } else if (user_role === "ROLE_DOCTOR") {
+                this.$router.push({
+                    name: "DoctorPage"
+                });
+            } else if (user_role === "ROLE_NURSE") {
+                this.$router.push({
+                    name: "NursePage"
+                });
+            } else if (user_role === "ROLE_PATIENT") {
+                this.$router.push({
+                    name: "PatientPage"
+                });
+            } else {
+                alert("Something went wrong. This should not happen.")
+                // this.$store.dispatch("clearStore")
+                // this.$router.push("/");
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,16 +1,31 @@
 <template>
-    <div>
-        <h1>This is NotFoundPage</h1>
-        <p>TODO: Add link/button to home page</p>
+    <div class="center">
+        <h1 class="blue-grey--text">Error 404 not found</h1>
+        <h2 class="blue-grey--text">Zatražili ste nepostojeći resurs.</h2>
+        <img 
+        src="../assets/bbone.png"
+        alt="Čak i slika ne postoji."
+        width="200"
+        height="200">
+        <ReturnHome/>
     </div>
 </template>
 
 <script>
+import ReturnHome from "../components/ReturnHome"
 export default {
-
+    name: "NotFound",
+    components: {
+        ReturnHome
+    }
 }
 </script>
 
 <style scoped>
-
+    .center {
+        position: fixed; /* or absolute */
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
 </style>
