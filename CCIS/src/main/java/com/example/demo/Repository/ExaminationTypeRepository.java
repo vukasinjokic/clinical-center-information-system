04ca099b3a263b3316ed551f8e0c3a5c78965b06
Nikola <nstojanovic998@gmail.com>
2020-06-14 12:27:05 +0200

    Sredjivanje sonarCloud bagova

--- CCIS/src/main/java/com/example/demo/Repository/ExaminationTypeRepository.java
+++ CCIS/src/main/java/com/example/demo/Repository/ExaminationTypeRepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.Repository;
 
-import com.example.demo.model.ClinicAdmin;
 import com.example.demo.model.ExaminationType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ExaminationTypeRepository extends JpaRepository<ExaminationType,Integer> {
 
